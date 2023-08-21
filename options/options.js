806ea17fc524b1188a069d07c5918692eac471ff
Crowdbotics <engineering@crowdbotics.com>
2023-08-21 14:06:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hugo-app-enterpris-110139",
+  url: "https://hugo-app-enterpris-110139.botics.co",
+  api: "https://hugo-app-enterpris-110139.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
